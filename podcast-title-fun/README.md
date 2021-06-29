d2209c6cbc409cb9a31e61dbed6961df7afd87cd
Sam Vevang <sam.vevang@gmail.com>
2021-06-29 13:00:03 -0500

    Init podcast title fun

---
+++ podcast-title-fun/README.md
@@ -0,0 +1,6 @@
+Welcome!
+========
+
+To get started open 'index.html' in your browser.
+
+Happy coding!
---
+++ podcast-title-fun/challenge_functions.js
@@ -0,0 +1,12 @@
+// Takes in an Array of podcast title strings
+// Returns an Array of strings where the words are reversed
+function reverseWords(podcastTitles){
+  //TODO return list where title "Some Fine Podcast" becomes "Podcast Fine Some"
+}
+
+// Takes in an Array of podcast title strings
+// Returns an Array of strings are lowercased and duplicate characters removed.
+function removeDupeChars(podcastTitles){
+  // TODO "Some Fine Podcast" becomes "some fin pdcat"
+}
+
